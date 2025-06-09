9dcd098777e4a90c4a1f62edf556f0fe1fc21ced
Jeremiah <sentomerojeremy@gmail.com>
2025-06-09 18:57:09 +0300

    initalise project setup

---
+++ src/test/java/com/ics/distributed_drinks_system/DistributedDrinksSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ics.distributed_drinks_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedDrinksSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
